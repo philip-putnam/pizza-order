028ba55c0e256afa2407a7d70720cef041a6daf5
Philip Putnam <putnamp989@gmail.com>
2017-01-27 10:46:56 -0800

    implement third specification, add orderPrice Order prototype

--- js/scripts.js
+++ js/scripts.js
@@ -18,6 +18,34 @@ Order.prototype.addPizzaToOrder = function(pizza) {
   this.pizzas.push(pizza);
 }
 
+Order.prototype.orderPrice = function() {
+  var orderTotal = 0;
+  // An array "sizePrices" holds the size to price key-value pairs
+  var sizePrices = [
+    {pizzaSize: "Small", price: 6},
+    {pizzaSize: "Medium", price: 8},
+    {pizzaSize: "Large", price: 10}
+  ];
+  var meatToppings = ["pepperoni", "sasauge", "bacon", "salami", "ground beef"]
+
+  // For loop, iterates through the "sizePrices" array
+  for (var index = 0; index < sizePrices.length; index++) {
+    if (this.pizzas[0].pizzaSize === sizePrices[index].pizzaSize) {
+      orderTotal += sizePrices[index].price;
+    }
+  }
+
+  for (var index = 0; index < meatToppings.length; index++) {
+    if (this.pizzas[0].pizzaTopping === meatToppings[index]) {
+      orderTotal += 2.5;
+    } else {
+      orderTotal += 1;
+    }
+  }
+
+  return orderTotal;
+}
+
 // Front-end, user-interface logic
 
 $(document).ready(function() {
@@ -30,6 +58,8 @@ var userPizza = new Pizza();
     userPizza.createPizza(userPizzaSize, userPizzaTopping);
     userOrder.addPizzaToOrder(userPizza);
     $(".col-md-4").append(userOrder.pizzas[0].pizzaSize + " " + userOrder.pizzas[0].pizzaTopping + " pizza");
+    $(".col-md-4").append("Total: $" + userOrder.orderPrice());
   })
+
 
 });
